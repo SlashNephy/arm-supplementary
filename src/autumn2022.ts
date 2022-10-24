a8df69dd51a7639541e93b0f9de7cd13139cc6fd
SlashNephy <spica@starry.blue>
2022-10-24 20:18:28 +0900

    2022秋アニメを補完する

---
+++ src/autumn2022.ts
@@ -0,0 +1,470 @@
+import type { ArmEntry } from '../lib/arm'
+
+export const autumn2022: ArmEntry[] = [
+  // TV: SPY×FAMILY 第2クール
+  {
+    annict_id: 9720,
+    mal_id: 50602,
+    syobocal_tid: 6451,
+  },
+  // TV: チェンソーマン
+  {
+    annict_id: 7869,
+    mal_id: 44511,
+    syobocal_tid: 6454,
+  },
+  // TV: ぼっち・ざ・ろっく！
+  {
+    annict_id: 8128,
+    mal_id: 47917,
+    syobocal_tid: 6474,
+  },
+  // TV: 機動戦士ガンダム 水星の魔女
+  {
+    annict_id: 8888,
+    mal_id: 49828,
+    syobocal_tid: 6466,
+  },
+  // TV: うる星やつら (2022)
+  {
+    annict_id: 9251,
+    mal_id: 50710,
+    syobocal_tid: 6480,
+  },
+  // TV: アキバ冥途戦争
+  {
+    annict_id: 9715,
+    mal_id: 52193,
+    syobocal_tid: 6448,
+  },
+  // TV: ヤマノススメ Next Summit
+  {
+    annict_id: 8004,
+    mal_id: 48491,
+    syobocal_tid: 6430,
+  },
+  // TV: ポプテピピック 第二シリーズ
+  {
+    annict_id: 9235,
+    mal_id: 50663,
+    syobocal_tid: 6438,
+  },
+  // TV: 陰の実力者になりたくて！
+  {
+    annict_id: 8138,
+    mal_id: 48316,
+    syobocal_tid: 6452,
+  },
+  // MOVIE: 劇場版 ソードアート・オンライン -プログレッシブ- 冥き夕闇のスケルツォ
+  {
+    annict_id: 9061,
+    mal_id: 50275,
+  },
+  // TV: 僕のヒーローアカデミア (第6期)
+  {
+    annict_id: 9123,
+    mal_id: 49918,
+    syobocal_tid: 6473,
+  },
+  // TV: 宇崎ちゃんは遊びたい！ω
+  {
+    annict_id: 7702,
+    mal_id: 42962,
+    syobocal_tid: 6439,
+  },
+  // TV: Do It Yourself!! -どぅー・いっと・ゆあせるふ-
+  {
+    annict_id: 8175,
+    mal_id: 48542,
+    syobocal_tid: 6455,
+  },
+  // TV: 転生したら剣でした
+  {
+    annict_id: 8946,
+    mal_id: 49891,
+    syobocal_tid: 6443,
+  },
+  // TV: ブルーロック
+  {
+    annict_id: 8830,
+    mal_id: 49596,
+    syobocal_tid: 6477,
+  },
+  // MOVIE: すずめの戸締まり
+  {
+    annict_id: 9207,
+    mal_id: 50594,
+  },
+  // TV: ゴールデンカムイ 第4期
+  {
+    annict_id: 9186,
+    mal_id: 50528,
+    syobocal_tid: 6441,
+  },
+  // TV: モブサイコ100 III
+  {
+    annict_id: 9011,
+    mal_id: 50172,
+    syobocal_tid: 6442,
+  },
+  // TV: ジョジョの奇妙な冒険 ストーンオーシャン 第2期
+  {
+    annict_id: 9482,
+    mal_id: 51367,
+    syobocal_tid: 6450,
+  },
+  // TV: BLEACH 千年血戦篇
+  {
+    annict_id: 7401,
+    mal_id: 41467,
+    syobocal_tid: 6487,
+  },
+  // TV: 4人はそれぞれウソをつく
+  {
+    annict_id: 9508,
+    mal_id: 51464,
+    syobocal_tid: 6456,
+  },
+  // TV: 新米錬金術師の店舗経営
+  {
+    annict_id: 8935,
+    mal_id: 49849,
+    syobocal_tid: 6446,
+  },
+  // TV: 不滅のあなたへ Season2
+  {
+    annict_id: 8849,
+    mal_id: 49709,
+    syobocal_tid: 6486,
+  },
+  // TV: 勇者パーティーを追放されたビーストテイマー、最強種の猫耳少女と出会う
+  {
+    annict_id: 9697,
+    mal_id: 52046,
+    syobocal_tid: 6444,
+  },
+  // MOVIE: 私に天使が舞い降りた！プレシャス・フレンズ
+  {
+    annict_id: 8122,
+    mal_id: 44141,
+  },
+  // TV: 悪役令嬢なのでラスボスを飼ってみました
+  {
+    annict_id: 9069,
+    mal_id: 49979,
+    syobocal_tid: 6447,
+  },
+  // TV: うちの師匠はしっぽがない
+  {
+    annict_id: 8808,
+    mal_id: 49533,
+    syobocal_tid: 6459,
+  },
+  // TV: 農民関連のスキルばっか上げてたら何故か強くなった。
+  {
+    annict_id: 9397,
+    mal_id: 51128,
+    syobocal_tid: 6457,
+  },
+  // TV: 夫婦以上、恋人未満。
+  {
+    annict_id: 9127,
+    mal_id: 50425,
+    syobocal_tid: 6445,
+  },
+  // TV: 不徳のギルド
+  {
+    annict_id: 9424,
+    mal_id: 51212,
+    syobocal_tid: 6462,
+  },
+  // TV: 名探偵コナン 犯人の犯沢さん
+  {
+    annict_id: 8980,
+    mal_id: 50010,
+    syobocal_tid: 6491,
+  },
+  // TV: 恋愛フロップス
+  {
+    annict_id: 9467,
+    mal_id: 51403,
+    syobocal_tid: 6458,
+  },
+  // MOVIE: 劇場版 転生したらスライムだった件 紅蓮の絆編
+  {
+    annict_id: 8940,
+    mal_id: 49877,
+  },
+  // TV: 令和のデ・ジ・キャラット
+  {
+    annict_id: 8786,
+    mal_id: 49179,
+    syobocal_tid: 6502,
+  },
+  // TV: PUI PUI モルカー DRIVING SCHOOL
+  {
+    annict_id: 9683,
+    mal_id: 51986,
+  },
+  // TV: 後宮の烏
+  {
+    annict_id: 9205,
+    mal_id: 50590,
+    syobocal_tid: 6468,
+  },
+  // TV: 虫かぶり姫
+  {
+    annict_id: 9300,
+    mal_id: 50923,
+    syobocal_tid: 6460,
+  },
+  // TV: 「艦これ」いつかあの海で
+  {
+    annict_id: 9479,
+    mal_id: 30455,
+    syobocal_tid: 6515,
+  },
+  // WEB: トニカクカワイイ 〜制服〜
+  {
+    annict_id: 9076,
+    mal_id: 51533,
+  },
+  // TV: ある朝、ダミーヘッドマイクになっていた俺クンの人生
+  {
+    annict_id: 9737,
+    mal_id: 52381,
+    syobocal_tid: 6479,
+  },
+  // MOVIE: かがみの孤城
+  {
+    annict_id: 9393,
+    mal_id: 51116,
+  },
+  // TV: 弱虫ペダル LIMIT BREAK
+  {
+    annict_id: 9195,
+    mal_id: 50552,
+    syobocal_tid: 6429,
+  },
+  // TV: ハーレムきゃんぷっ！
+  {
+    annict_id: 9821,
+    mal_id: 52717,
+    syobocal_tid: 6483,
+  },
+  // TV: クールドジ男子
+  {
+    annict_id: 9580,
+    mal_id: 51680,
+    syobocal_tid: 6469,
+  },
+  // TV: 忍の一時
+  {
+    annict_id: 9386,
+    mal_id: 51098,
+    syobocal_tid: 6461,
+  },
+  // MOVIE: THE FIRST SLAM DUNK
+  {
+    annict_id: 7926,
+    mal_id: 45649,
+  },
+  // WEB: うまゆる
+  {
+    annict_id: 9631,
+    mal_id: 51772,
+    syobocal_tid: 6512,
+  },
+  // MOVIE: かぐや様は告らせたい-ファーストキッスは終わらない-
+  {
+    annict_id: 9717,
+  },
+  // TV: 魔入りました！入間くん 第3シリーズ
+  {
+    annict_id: 8883,
+    mal_id: 49784,
+    syobocal_tid: 6489,
+  },
+  // MOVIE: 僕が愛したすべての君へ
+  {
+    annict_id: 8948,
+    mal_id: 49834,
+  },
+  // WEB: TIGER & BUNNY 2 part2
+  {
+    annict_id: 9827,
+    mal_id: 41595,
+  },
+  // TV: マブラヴ オルタネイティヴ 第2期
+  {
+    annict_id: 9229,
+    mal_id: 50638,
+    syobocal_tid: 6490,
+  },
+  // TV: ピーター・グリルと賢者の時間 Super Extra
+  {
+    annict_id: 9085,
+    mal_id: 50348,
+    syobocal_tid: 6493,
+  },
+  // MOVIE: 君を愛したひとりの僕へ
+  {
+    annict_id: 8949,
+    mal_id: 49835,
+  },
+  // TV: 聖剣伝説 LEGEND OF MANA -The Teardrop Crystal-
+  {
+    annict_id: 8660,
+    mal_id: 49304,
+    syobocal_tid: 6478,
+  },
+  // TV: アークナイツ【黎明前奏/PRELUDE TO DAWN】
+  {
+    annict_id: 9031,
+    mal_id: 50205,
+    syobocal_tid: 6500,
+  },
+  // TV: ベルセルク 黄金時代篇 MEMORIAL EDITION
+  {
+    annict_id: 9822,
+    mal_id: 52976,
+    syobocal_tid: 6488,
+  },
+  // MOVIE: ぼくらのよあけ
+  {
+    annict_id: 9436,
+    mal_id: 51307,
+  },
+  // TV: ヒューマンバグ大学 -不死学部不幸学科-
+  {
+    annict_id: 9623,
+    mal_id: 53012,
+    syobocal_tid: 6470,
+  },
+  // TV: 永久少年 Eternal Boys
+  {
+    annict_id: 9431,
+    mal_id: 51293,
+    syobocal_tid: 6481,
+  },
+  // TV: メガトン級ムサシ シーズン2
+  {
+    annict_id: 9198,
+    mal_id: 50559,
+    syobocal_tid: 6492,
+  },
+  // TV: 羅小黒戦記 ぼくが選ぶ未来 TV放送版
+  {
+    annict_id: 10113,
+    syobocal_tid: 6061,
+  },
+  // TV: アイドリッシュセブン Third BEAT! 第2クール
+  {
+    annict_id: 7902,
+    mal_id: 46654,
+    syobocal_tid: 6440,
+  },
+  // OVA: 女の園の星
+  {
+    annict_id: 9813,
+  },
+  // WEB: ロマンティック・キラー
+  {
+    annict_id: 9837,
+    mal_id: 52865,
+  },
+  // TV: 女体化した僕を騎士様達がねらってます
+  {
+    annict_id: 10091,
+    syobocal_tid: 6482,
+  },
+  // TV: Fate/strange Fake -Whispers of Dawn-
+  {
+    annict_id: 10097,
+  },
+  // TV: 僕とロボコ
+  {
+    annict_id: 9678,
+    mal_id: 51940,
+  },
+  // MOVIE: 銀河英雄伝説 Die Neue These 策謀 第三章
+  {
+    annict_id: 9654,
+    mal_id: 51805,
+  },
+  // TV: VAZZROCK THE ANIMATION
+  {
+    annict_id: 7737,
+    mal_id: 43771,
+    syobocal_tid: 6476,
+  },
+  // MOVIE: 銀河英雄伝説 Die Neue These 策謀 第二章
+  {
+    annict_id: 9653,
+    mal_id: 51805,
+  },
+  // WEB: 七つの大罪 怨嗟のエジンバラ 前編
+  {
+    annict_id: 9082,
+    mal_id: 50315,
+  },
+  // WEB: 夜は猫といっしょ(秋クール)
+  {
+    annict_id: 10117,
+    mal_id: 51692,
+    syobocal_tid: 6511,
+  },
+  // OVA: 超次元ゲイム ネプテューヌ ～ねぷねぷだらけの感謝祭 (フェスティバル)～
+  {
+    annict_id: 8997,
+  },
+  // WEB: exception
+  {
+    annict_id: 8679,
+    mal_id: 49163,
+  },
+  // TV: ぷにるんず
+  {
+    annict_id: 10089,
+    mal_id: 52935,
+    syobocal_tid: 6499,
+  },
+  // MOVIE: 劇場版アルゴナビス AXIA
+  {
+    annict_id: 7925,
+    mal_id: 45731,
+  },
+  // TV: シルバニアファミリー フレアのハッピーダイアリー
+  {
+    annict_id: 10085,
+    mal_id: 52964,
+    syobocal_tid: 6485,
+  },
+  // TV: チキップダンサーズ 第2期
+  {
+    annict_id: 10086,
+    syobocal_tid: 6465,
+  },
+  // TV: 湖池屋SDGs劇場 サスとテナ シーズン3
+  {
+    annict_id: 10106,
+  },
+  // MOVIE: 映画かいけつゾロリ ラララ♪スターたんじょう
+  {
+    annict_id: 9579,
+  },
+  // WEB: スター・ウォーズ: バッド・バッチ シーズン2
+  {
+    annict_id: 9679,
+  },
+  // TV: おばけずかん！
+  {
+    annict_id: 10084,
+    mal_id: 52319,
+  },
+  // TV: アニメ ボス・ベイビー シーズン2
+  {
+    annict_id: 10095,
+    syobocal_tid: 6496,
+  },
+]
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { arm } from '@kawaiioverflow/arm'
 
+import { autumn2022 } from './autumn2022'
+
 import type { ArmEntry } from '../lib/arm'
 
 export const entries = (): ArmEntry[] => {
-  return [...arm]
+  return [...arm, ...autumn2022]
 }
