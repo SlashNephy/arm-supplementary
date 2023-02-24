f8f39378ec1cea88798f982d022f0d2b3f721f4f
SlashNephy <SlashNephy@users.noreply.github.com>
2023-02-24 14:03:16 +0000

    🪄 update database

--- src/annict.ts
+++ src/annict.ts
@@ -29036,7 +29036,7 @@ export const annict: ArmEntry[] = [
   // [2010-SUMMER] MOVIE: CINEMA KEIBA/JAPAN WORLD CUP
   // https://annict.com/works/10179
   { annict_id: 10179, mal_id: 37326 },
-  // [2023-null] TV: 絆のアリル
+  // [2023-SPRING] TV: 絆のアリル
   // https://annict.com/works/10180
   { annict_id: 10180, mal_id: 51139 },
   // [2016-SPRING] WEB: リック・アンド・モーティ シーズン1
